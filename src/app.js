f5790617ee594887df5d66f9cf2c78e0a546e5bc
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-05-21 00:41:28 +0500

    short params, update utils for geo & weather fetch

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 
-const { geoCodeAddressPromise } = require("./utils/geoCode");
-const { fetchWeatherPromise } = require("./utils/weather");
+const { geoCodeAddress } = require("./utils/geoCode");
+const { getWeather } = require("./utils/weatherStack");
 
 const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 3000;
@@ -16,61 +16,60 @@ app.use(express.static(path.join(__dirname, "../public/")));
 
 const defaultProps = {
   title: "Page Title",
-  author: "Asjad"
+  author: "Asjad",
 };
 
-app.get("/", (request, response) => {
-  response.render(
+app.get("/", (_, res) => {
+  res.render(
     "index.hbs",
     Object.assign(defaultProps, { title: "Weather App" })
   );
 });
 
-app.get("/about", (request, response) => {
-  response.render("about.hbs", Object.assign(defaultProps, { title: "About" }));
+app.get("/about", (_, res) => {
+  res.render("about.hbs", Object.assign(defaultProps, { title: "About" }));
 });
 
-app.get("/help", (request, response) => {
-  response.render(
+app.get("/help", (_, res) => {
+  res.render(
     "help",
     Object.assign(defaultProps, {
       title: "Help",
-      message: "Help Route"
+      message: "Help Route",
     })
   );
 });
 
-app.get("/help/*", (request, response) => {
-  response.render(
+app.get("/help/*", (_, res) => {
+  res.render(
     "404",
     Object.assign(defaultProps, {
-      title: "Help Not Found"
+      title: "Help Not Found",
     })
   );
 });
 
-app.get("/weather", (request, response) => {
-  if (!request.query.address) {
-    return response.status(400).json({ error: "missing address!" });
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.status(400).json({ error: "missing address!" });
   }
 
-  (async queryAddress => {
+  (async (queryAddress) => {
     try {
-      let address = await geoCodeAddressPromise(queryAddress);
-      let weather = await fetchWeatherPromise(address.lat, address.lng);
-
-      return response.json({ address, weather });
+      let address = await geoCodeAddress(queryAddress);
+      let weather = await getWeather(address.lat, address.lng);
+      return res.json({ address, weather });
     } catch (error) {
-      return response.status(500).json({ error });
+      return res.status(500).json({ error });
     }
-  })(request.query.address);
+  })(req.query.address);
 });
 
-app.get("*", (request, response) => {
-  response.render(
+app.get("*", (_, res) => {
+  res.render(
     "404",
     Object.assign(defaultProps, {
-      title: "Page Not Found"
+      title: "Page Not Found",
     })
   );
 });
